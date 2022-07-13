93b69fbfb0ea2ebc4610ff446db7c2aea7a8f5f0
Marco <fernstaedtmarco@gmail.com>
2022-07-13 11:34:47 -0700

    updated computerPlay fuction

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // computer picks rock, paper or scisors. randomly seleted from array output: rock
 const computerPlay = () => {
+    // options for computer to choose
     const options = ["Rock", "Paper", "Scissors"];
+    // selection an option at random.
     const randomOption = Math.floor(Math.random() * options.length)
+    // assigning random option to a variable.
     const computerChoice = options[randomOption]
+    // returing the variable.
     return computerChoice;
 }
-
-console.log(computerPlay())
