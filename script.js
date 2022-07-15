f212510fd91bc99f4dc1fb585703339bf7c9690c
Marco <fernstaedtmarco@gmail.com>
2022-07-15 09:34:55 -0700

    finished game function to start and end game, now working on keeping score

--- script.js
+++ script.js
@@ -10,23 +10,46 @@ const computerPlay = () => {
     return computerChoice;
 }
 
-const computerSelected = computerPlay()
-const playerSelected = "rock"
+// keepp track of player score
+let playerScore = 0
+let computerScore = 0
 
 // plays through a round
 const playRound = (computerSelected, playerSelected) => {
     // to prevent case sensitive errors
     playerSelected.toLowerCase()
+
     // all possible scenarios which player wins, if player loses else statment takes over
     if (playerSelected === "rock" && computerSelected === "scissors") {
+        playerScore++
         return `You Win! ${playerSelected} beats ${computerSelected}`
     } else if (playerSelected === "paper" && computerSelected === "rock") {
+        playerScore++
         return `You Win! ${playerSelected} beats ${computerSelected}`
     } else if (playerSelected === "scissors" && computerSelected === "paper") {
+        playerScore++
         return `You Win! ${playerSelected} beats ${computerSelected}`
     } else if (playerSelected === computerSelected) {
         return `Tie! better luck next time`
     } else {
+        computerScore++
         return `You Lose! ${computerSelected} beats ${playerSelected}`
     }
 }
+
+const game = () => {
+    for (let rounds = 0; rounds < 5; rounds++) {
+        const computerSelected = computerPlay()
+        const playerSelected = prompt("Please select Rock, Paper or Scissors")
+
+        console.log(playRound(computerSelected, playerSelected))
+        if (playerScore > computerScore) {
+            return console.log(`The Winner is You! with a score of ${playerScore}`)
+        } else {
+            return console.log(`The Winner is Computer with a score of ${computerScore}`)
+        }
+    }
+    retur
+}
+
+game()
